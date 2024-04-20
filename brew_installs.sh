0afcb18625da2059389039803503bb883c1ca901
Carter Mullenix <carter.mullenix@hotmail.com>
2024-04-19 21:27:40 -0400

    Adding a bash script of brew installs

---
+++ brew_installs.sh
@@ -0,0 +1,107 @@
+#!bin/bash
+
+
+# normal brew installs
+brew install boost
+brew install brotli
+brew install c-ares
+brew install cairo
+brew install cask
+brew install coreutils
+brew install docker
+brew install docker-compose-completion
+brew install edencommon
+brew install emacs
+brew install fb303
+brew install fbthrift
+brew install fizz
+brew install fmt
+brew install folly
+brew install fontconfig
+brew install freetype
+brew install fzf
+brew install gcc
+brew install gettext
+brew install gflags
+brew install git
+brew install glib
+brew install glog
+brew install gmp
+brew install gnutls
+brew install icu4c
+brew install isl
+brew install jansson
+brew install jpeg-turbo
+brew install jq
+brew install libevent
+brew install libgit2
+brew install libidn2
+brew install libmpc
+brew install libnghttp2
+brew install libpng
+brew install libsodium
+brew install libssh2
+brew install libtasn1
+brew install libtermkey
+brew install libuv
+brew install libvterm
+brew install libx11
+brew install libxau
+brew install libxcb
+brew install libxdmcp
+brew install libxext
+brew install libxrender
+brew install llvm
+brew install luajit
+brew install luv
+brew install lz4
+brew install lzo
+brew install mpdecimal
+brew install mpfr
+brew install msgpack
+brew install ncurses
+brew isntall neofetch
+brew install neovim
+brew install nettle
+brew install node
+brew install oh-my-posh
+brew install oniguruma
+brew install openblas
+brew install openssl@3
+brew install p11-kit
+brew install pcre2
+brew install perl
+brew install pixman
+brew install pkg-config
+brew install r
+brew install readline
+brew install rust
+brew install screenresolution
+brew install snappy
+brew install spicetify-cli
+brew install sqlite
+brew install stow
+brew install tcl-tk
+brew install tmux
+brew install tree-sitter
+brew install unbound
+brew install unibilium
+brew install utf8proc
+brew isntall wangle
+brew install watchman
+brew install xorgproto
+brew install xz
+brew install yabai
+brew install yarn
+brew install z3
+brew install zsh-syntax-highlighting
+brew install zstd
+
+# Install cask
+brew install --cask alacritty
+brew install --cask discord
+brew install --cask kitty
+brew install --cask rstudio
+brew install --cask spotify
+brew install --cask visual-studio-code
+brew install --cask google-chrome
